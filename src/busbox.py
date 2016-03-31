633a16936e505ea156d7e274459d4a9df868acae
Daniel Streby <dstreby@2u.com>
2016-03-31 15:17:30 -0400

    Classed it up

--- src/busbox.py
+++ src/busbox.py
@@ -3,10 +3,36 @@
 import argparse
 import requests
 import json
+import time
 from datetime import datetime
 from myconfig import API_KEY
 
-url = 'http://bustime.mta.info/api/siri/stop-monitoring.json'
+class Bus_info:
+  def __init__(self, monitored_stop_visit):
+    try:
+      ugly_time = monitored_stop_visit['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
+      arriv_time = datetime.strptime(ugly_time[0:19], '%Y-%m-%dT%H:%M:%S')
+      delta_time = arriv_time - datetime.now()
+      self.time_wait = int(round((delta_time.total_seconds() / 60)))
+      self.distance = monitored_stop_visit['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
+    except:
+      self.time_wait = 'UNKNOWN'
+      self.distance = monitored_stop_visit['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
+
+class Bus_time:
+  def __init__(self, stop_num):
+    url = 'http://bustime.mta.info/api/siri/stop-monitoring.json'
+    keys = {'key' : API_KEY, 'OperatorRef' : 'MTA', 'MonitoringRef' : stop_num}
+    bus_data_raw = requests.get(url, params=keys)
+    self.bus_data =  bus_data_raw.json()
+    self.stop_name = self.bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']
+    self.dest_name = self.bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['DestinationName']
+    self.results = len(self.bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']) 
+
+
+#############################
+# Setup Arg Parser          #
+#############################
 
 parser = argparse.ArgumentParser(description='Get info on busses')
 
@@ -15,29 +41,29 @@ parser.add_argument('-s',
     action = 'store',
     default = '404137',
     help = 'The bus stop ID code')
+parser.add_argument('-c',
+    action = 'store_true',
+    help = 'Countdown to next bus')
 
 args = parser.parse_args()
 
-keys = {'key' : API_KEY, 'OperatorRef' : 'MTA', 'MonitoringRef' : args.stop}
+#############################
+# Begin actual program      #
+#############################
+stop_num = args.stop
 
-bus_data_raw = requests.get(url, params=keys)
-bus_data = bus_data_raw.json()
-
-stop_name = bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']
-dest_name = bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0]['MonitoredVehicleJourney']['DestinationName']
-results = len(bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']) 
-
-print("There are %d busses en route to stop %s heading to %s:" % (results, stop_name, dest_name))
-
-for b in bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']:
+if args.c:
   try:
-    ugly_time = b['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
-    arriv_time = datetime.strptime(ugly_time[0:19], '%Y-%m-%dT%H:%M:%S')
-    delta_time = arriv_time - datetime.now()
-    time_wait = int(round((delta_time.total_seconds() / 60)))
-    distance = b['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
-    print("%s arriving in approx %s minutes" % distance, time_wait)
-  except:
-    distance = b['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
-    print("%s (no ETA available)" % distance)
-
+    while True:
+      bus_time = Bus_time(stop_num)
+      bus_info = Bus_info(bus_time.bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit'][0])
+      print("%s arriving in approx %s minutes" % (bus_info.distance, bus_info.time_wait), end='\r')
+      time.sleep(60)
+  except KeyboardInterrupt:
+    print("\nExiting")
+else:
+  bus_time = Bus_time(stop_num)
+  print("There are %d busses en route to stop %s heading to %s:" % (bus_time.results, bus_time.stop_name, bus_time.dest_name))
+  for b in bus_time.bus_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']:
+    bus_info = Bus_info(b)
+    print("%s arriving in approx %s minutes" % (bus_info.distance, bus_info.time_wait))
